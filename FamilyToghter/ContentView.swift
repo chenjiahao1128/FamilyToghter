ba42c7d563ef532556316672d246267f12e139d8
cx.zhang <dev+cx.zhang@startiasoft.co.jp>
2022-06-19 20:52:09 +0900

    Initial Commit

---
+++ FamilyToghter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FamilyToghter
+//
+//  Created by 張  春雪 on 2022/06/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FamilyToghter/FamilyToghterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyToghterApp.swift
+//  FamilyToghter
+//
+//  Created by 張  春雪 on 2022/06/19.
+//
+
+import SwiftUI
+
+@main
+struct FamilyToghterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
